f455b5029be708d801028f0caa42cf66fa6f8740
leonardo reis <leonardo92reis@gmail.com>
2024-05-07 18:01:37 -0300

    update information

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/leonardo9245/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,6 @@ function updateProfileInfo(profileData) {
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
 
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
-
     const location = document.getElementById('profile.location')
     location.innerText = profileData.location
 
